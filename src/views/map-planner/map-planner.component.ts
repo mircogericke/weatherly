6b46a9c5e30c85bed90fa688ecc968ca38842a87
Mirco Gericke <foss@mircogericke.com>
2025-01-19 20:34:34 +0100

    fixed missing icon positions

--- src/views/map-planner/map-planner.component.ts
+++ src/views/map-planner/map-planner.component.ts
@@ -13,6 +13,11 @@ Marker.prototype.options.icon = icon({
 	iconRetinaUrl: 'media/marker-icon-2x.png',
 	iconUrl: 'media/marker-icon.png',
 	shadowUrl: 'media/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
 });
 
 @Component({
